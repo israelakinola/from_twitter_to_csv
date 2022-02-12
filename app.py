055a06a7d7c025ae50e6d3929d06ac8e0de4b069
Tega O <Kelv_leo@MacTega.local>
2022-02-12 11:54:27 -0500

    third commit

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_file
 import tweepy
 import csv
 
 
 app = Flask(__name__)
 
-john = ''
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    fetched = False
     if request.method == 'POST' and request.form['hashtag'] != '' :
-        if(fetch(request.form['hashtag'])):
+        try:
+            fetch(request.form['hashtag'])
+        except:
+            pass
+        finally:
             return render_template("index.html", hashtag = request.form['hashtag'], fetched = True)
     return render_template("index.html")
 
@@ -21,7 +23,7 @@ def fetch(hashtag):
     # input your credentials here
     hashtag = hashtag
     auth = tweepy.OAuth2BearerHandler("AAAAAAAAAAAAAAAAAAAAAO2jZAEAAAAAeHKHp0U%2FLLbThLbR94yZREvpJ54%3Dc2S9jqbpzhQYhrGHa9w8cT8Td7YHMtlsVUmr8C1Tf4vXnoqEBD")
-    api = tweepy.API(auth, wait_on_rate_limit=True, sleep_on_rate_limit=False)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
 
     # Open/Create a file to append data
     csvFile = open('tweets.csv', 'a')
@@ -31,8 +33,12 @@ def fetch(hashtag):
 
     for tweet in tweepy.Cursor(api.search_tweets,q=hashtag,
                             lang="en").items(20):
-                            # print (tweet.created_at, tweet.text)
                             csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+
+@app.route('/download', methods=['GET', 'POST'])
+def download():    
+    path = "/Users/Kelv_leo/Desktop/fetchtocsv/tweets.csv"
+    return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
     app.debug = True
