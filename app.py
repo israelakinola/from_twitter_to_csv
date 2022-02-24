ad3a2120b020a738d7416302024db71bf459ddc6
Unknown User <Kelv_leo@MacTega.local>
2022-02-23 19:39:14 -0500

    change hastag to keyword

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ bearer_token = ''
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
+    """
+        This function return the Index HTML Template and its form handling
+    Return
+    ______
+        HTML Template
+
+    """
     if request.method == 'POST' and request.form['hashtag'] != '' :
         try:
             fetch(request.form['hashtag'], int(request.form['qty']))
@@ -21,8 +28,19 @@ def index():
     return render_template("index.html")
 
 
-def fetch(hashtag, qty):
-    auth = tweepy.OAuth2BearerHandler(bearer_auth)
+def fetch(keyword, qty):
+    """
+        This function fetch the tweets and write it to a CSV file.
+
+    Arguments
+    _________
+        keyword <String> : Users keyword that need to be fetched
+        qty <Int> : User inputed amount of tweets that need to be fetched
+        
+
+
+    """
+    auth = tweepy.OAuth2BearerHandler(bearer_token)
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
     # Open/Create a file to append dataß
@@ -31,15 +49,26 @@ def fetch(hashtag, qty):
     #Use csv Writer
     csvWriter = csv.writer(csvFile)
     csvWriter.writerow(['Created at', 'Username', 'Tweets', 'Retweet Count' 'Likes'])
-    for tweet in tweepy.Cursor(api.search_tweets,q=hashtag,
+    for tweet in tweepy.Cursor(api.search_tweets,q=keyword,
                             lang="en").items(qty):
                             csvWriter.writerow([tweet.created_at, tweet.user.screen_name, tweet.text.encode('utf-8'), tweet.retweet_count, tweet.favorite_count])
 
 @app.route('/download', methods=['GET', 'POST'])
 def download():    
+    """
+        This function handle the CSV File download
+
+    Return
+    _________
+        File Path <String>
+    """
     path = "/Users/Kelv_leo/Desktop/fetchtocsv/tweets.csv"
     return send_file(path, as_attachment=True)
 
+
 if __name__ == '__main__':
+    """
+        This function run the file if its the main file
+    """
     app.debug = True
     app.run()
