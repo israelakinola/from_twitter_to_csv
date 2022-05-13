fc04680853027088b69f66c7c39ea9ccc8c9ff14
Israel <gregsmedia2014@gmail.com>
2022-05-13 17:42:47 -0400

    Formartted with black

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-
-from flask import Flask, redirect, request, render_template, send_file,flash, url_for
+from flask import Flask, redirect, request, render_template, send_file, flash, url_for
 import tweepy
 import csv
 from os import getcwd
 
 
-
 app = Flask(__name__)
+
 app.secret_key = "auoesh.bouoastuh.43,uoausoehuosth3ououea.auoub!"
 
-#Input your Twitter Bearer Token here
+# Input your Twitter Bearer Token here
 bearer_token = ""
 
-download_file_path = f'{getcwd()}/downloads'
+download_file_path = f"{getcwd()}/downloads"
 
-@app.route("/", methods=['POST', 'GET'])
+
+@app.route("/", methods=["POST", "GET"])
 def index():
     """
         This function return the Index HTML Template and its form handling
@@ -23,21 +23,24 @@ def index():
         HTML Template
 
     """
-    if request.method == 'POST' and request.form['keyword'] != '': 
-        #Fetch tweets
-        if(validate_keyword):
-            tweets = fetch(request.form['keyword'], validate_qty(request.form['qty']))
-            #save to CSV
-            save_to_csv(tweets, request.form['keyword'])      
-            return render_template("index.html", keyword= request.form['keyword'], fetched = True)
+    if request.method == "POST" and request.form["keyword"] != "":
+        # Fetch tweets
+        if validate_keyword:
+            tweets = fetch(request.form["keyword"], validate_qty(request.form["qty"]))
+            # save to CSV
+            save_to_csv(tweets, request.form["keyword"])
+            return render_template(
+                "index.html", keyword=request.form["keyword"], fetched=True
+            )
     return render_template("index.html")
+
 
 def validate_qty(qty):
     """
         This function validate the Quantity of tweets a user inputed
     Arguments
     _________
-    qty: <int> Number of tweets users want to fetch. 
+    qty: <int> Number of tweets users want to fetch.
 
     Return
     ______
@@ -49,30 +52,31 @@ def validate_qty(qty):
     except ValueError:
         pass
     else:
-        converted= True
+        converted = True
 
-    #Check if qty was converted to 
+    # Check if qty was converted to
     if converted:
-        #check if int is greater than zero
+        # check if int is greater than zero
         if qty > 0:
-            return qty 
-    #Set qty to a default 0f 10 and return qty
+            return qty
+    # Set qty to a default 0f 10 and return qty
     qty = 10
     return qty
+
 
 def validate_keyword(keyword):
     """
         This function validate the Keyword of tweets a user inputed
     Arguments
     _________
-    keyword: <int> Number of tweets users want to fetch. 
+    keyword: <int> Number of tweets users want to fetch.
 
     Return
     ______
          Bool: <bool> True represent keyword was validated, False represent not validated
     """
-    #check if keyword length is greater than zero
-    if(len(keyword))<=0:
+    # check if keyword length is greater than zero
+    if (len(keyword)) <= 0:
         return False
     return True
 
@@ -85,7 +89,7 @@ def fetch(keyword, qty):
     _________
         keyword <String> : Users keyword that need to be fetched
         qty <Int> : User inputed amount of tweets that need to be fetched
-    
+
     Return
     ______
         Tweets Objects
@@ -94,10 +98,9 @@ def fetch(keyword, qty):
     auth = tweepy.OAuth2BearerHandler(bearer_token)
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
+    return tweepy.Cursor(api.search_tweets, q=keyword, lang="en").items(qty)
 
-    return tweepy.Cursor(api.search_tweets,q=keyword,
-                            lang="en").items(qty)
-    
+
 def save_to_csv(tweets, keyowrd_as_file_name):
     """
         This function loop through tweets objects and write them to a CSV file
@@ -108,18 +111,25 @@ def save_to_csv(tweets, keyowrd_as_file_name):
 
     """
     cwd = getcwd()
-     #Open/Create a file to append data
-    csvFile = open(f'{download_file_path}/{keyowrd_as_file_name}.csv', 'w')
-     #Use csv Writer
+    # Open/Create a file to append data
+    csvFile = open(f"{download_file_path}/{keyowrd_as_file_name}.csv", "w")
+    # Use csv Writer
     csvWriter = csv.writer(csvFile)
-    csvWriter.writerow(['Created at', 'Username', 'Tweets', 'Retweet Count', 'Likes'])
+    csvWriter.writerow(["Created at", "Username", "Tweets", "Retweet Count", "Likes"])
     for tweet in tweets:
-        csvWriter.writerow([tweet.created_at, tweet.user.screen_name, tweet.text.encode('utf-8'), tweet.retweet_count, tweet.favorite_count])
+        csvWriter.writerow(
+            [
+                tweet.created_at,
+                tweet.user.screen_name,
+                tweet.text.encode("utf-8"),
+                tweet.retweet_count,
+                tweet.favorite_count,
+            ]
+        )
 
 
-
-@app.route('/downloads/<keyowrd_as_file_name>', methods=['GET', 'POST'])
-def download(keyowrd_as_file_name):    
+@app.route("/downloads/<keyowrd_as_file_name>", methods=["GET", "POST"])
+def download(keyowrd_as_file_name):
     """
         This function handle the CSV File download
 
@@ -131,9 +141,9 @@ def download(keyowrd_as_file_name):
     return send_file(path, as_attachment=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
-        This function run the file if its the main file
+    This function run the file if its the main file
     """
     app.debug = True
     app.run()
--- test/test.py
+++ test/test.py
@@ -1,75 +1,73 @@
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import unittest
 import app
-import os 
+import os
 
 
-# browser= webdriver.Chrome()
-# browser.get("http://www.google.com")
-# print(browser.title)
 class TestApp(unittest.TestCase):
-    """ This class run the tests for the App Module """
+    """This class run the tests for the App Module"""
+
     browser = webdriver.Chrome()
 
     def test_validate_qty(self):
-        """ This method runs the test  for the validate_qty function in the App Module """
+        """This method runs the test  for the validate_qty function in the App Module"""
         self.assertEqual(app.validate_qty("100"), 100)
         self.assertEqual(app.validate_qty("-1"), 10)
         self.assertEqual(app.validate_qty("d"), 10)
 
     def test_validate_keyword(self):
-        """ This method runs the test for the validate_keyword function in the App Module """
-        self.assertFalse(app.validate_keyword(''))
-        self.assertTrue(app.validate_keyword('donald'))
-
+        """This method runs the test for the validate_keyword function in the App Module"""
+        self.assertFalse(app.validate_keyword(""))
+        self.assertTrue(app.validate_keyword("donald"))
 
     def test_index_view(self):
-        """ This method test the homepage view """
-         # Navigate to url
+        """This method test the homepage view"""
+        # Navigate to url
         self.browser.get("http://localhost:5000/")
         # Find if form element is present
         fetch_form_ele = self.browser.find_element(By.TAG_NAME, "form")
         self.assertTrue(fetch_form_ele)
         # Find input element by the name keyword and check if its present
-        keyword_ele = self.browser.find_element(By.NAME, 'keyword')
+        keyword_ele = self.browser.find_element(By.NAME, "keyword")
         self.assertTrue(keyword_ele)
         # Find input element by the name qty and check if its present
-        qty_ele = self.browser.find_element(By.NAME, 'qty')
+        qty_ele = self.browser.find_element(By.NAME, "qty")
         self.assertTrue(qty_ele)
         # Find fetch button button web element and check if its present
-        fetch_btn_ele = self.browser.find_element(By.ID, 'fetch_btn')
+        fetch_btn_ele = self.browser.find_element(By.ID, "fetch_btn")
         self.assertTrue(fetch_btn_ele)
-    
-   
 
     def test_fetch_form(self):
-        """ This method test the fetch form, check if the csv file is in the downloads folder and its downloadable on the browser """
+        """This method test the fetch form, check if the csv file is in the downloads folder and its downloadable on the browser"""
         # Navigate to url
         self.browser.get("http://localhost:5000/")
-       # Wait until browser element is clickable, find input element by the name keyword and send keys to it 
-        keys_value = 'hello'
-        download_file_path = f'{os.path.normpath(os.getcwd() + os.sep + os.pardir)}/downloads'
-        WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='keyword']"))).send_keys(keys_value)
+        # Wait until browser element is clickable, find input element by the name keyword and send keys to it
+        keys_value = "hello"
+        download_file_path = (
+            f"{os.path.normpath(os.getcwd() + os.sep + os.pardir)}/downloads"
+        )
+        WebDriverWait(self.browser, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='keyword']"))
+        ).send_keys(keys_value)
         # Find fetch button button web element and click it
-        self.browser.find_element(By.ID, 'fetch_btn').click()
+        self.browser.find_element(By.ID, "fetch_btn").click()
         # Find button element by the name download and check if its present
-        btn_ele = self.browser.find_elements(By.ID, 'download_btn')
+        btn_ele = self.browser.find_elements(By.ID, "download_btn")
         self.assertTrue(btn_ele)
         # Check if there is a csv file with the keyword name in the downloads directory
         csv_file = open(f"{download_file_path}/{keys_value}.csv")
         self.assertTrue(csv_file)
- 
-
 
     @classmethod
     def tearDownClass(cls):
         cls.browser.quit()
-        
+
 
 if __name__ == "__main__":
     unittest.main()
